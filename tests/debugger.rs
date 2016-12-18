d6aaa1591686a2fc2ee21ec7126173317e02e719
Markus Westerlind <marwes91@gmail.com>
2016-12-18 23:32:36 +0100

    feat: Handle step requests

--- tests/debugger.rs
+++ tests/debugger.rs
@@ -37,6 +37,22 @@ macro_rules! request {
     }
 }
 
+macro_rules! expect_response {
+    ($read: expr, $typ: ty, $name: expr) => { {
+        let msg: $typ = expect_message(&mut $read);
+        assert_eq!(msg.command, $name);
+        msg
+    } }
+}
+
+macro_rules! expect_event {
+    ($read: expr, $typ: ty, $event: expr) => { {
+        let event: $typ = expect_message(&mut $read);
+        assert_eq!(event.event, $event);
+        event
+    } }
+}
+
 lazy_static! {
     static ref PORT: Mutex<i32> = Mutex::new(4711);
 }
@@ -95,6 +111,7 @@ fn run_debugger<F>(f: F)
         seq,
         None
     };
+    expect_response!(read, DisconnectResponse, "disconnect");
 
     child.wait().unwrap();
 }
@@ -114,12 +131,65 @@ fn launch<W>(stream: W, seq: &mut i64, program: &str)
     };
 }
 
+fn request_debug_info(seq: &mut i64,
+                      stream: &TcpStream,
+                      mut read: &mut BufReader<&TcpStream>)
+                      -> (StackTraceResponse, ScopesResponse, VariablesResponse) {
+    request! {
+        stream,
+        ThreadsRequest,
+        "threads",
+        *seq,
+        None
+    };
+    expect_response!(read, ThreadsResponse, "threads");
+
+    request! {
+        stream,
+        StackTraceRequest,
+        "stackTrace",
+        *seq,
+        StackTraceArguments {
+            levels: Some(20),
+            start_frame: None,
+            thread_id: 1,
+        }
+    };
+    let trace = expect_response!(read, StackTraceResponse, "stackTrace");
+
+    request! {
+        stream,
+        ScopesRequest,
+        "scopes",
+        *seq,
+        ScopesArguments { frame_id: 0 }
+    };
+    let scopes = expect_response!(read, ScopesResponse, "scopes");
+
+    request! {
+        stream,
+        VariablesRequest,
+        "variables",
+        *seq,
+        VariablesArguments {
+            count: None,
+            filter: None,
+            start: None,
+            variables_reference: 1
+        }
+    };
+    let variables = expect_response!(read, VariablesResponse, "variables");
+    (trace, scopes, variables)
+}
+
 fn expect_message<M, R>(read: R) -> M
     where M: serde::Deserialize,
           R: BufRead,
 {
     let value = read_message(read).unwrap().unwrap();
-    from_str(&value).unwrap()
+    from_str(&value).unwrap_or_else(|err| {
+        panic!("{} in message:\n{}", err, value);
+    })
 }
 
 #[test]
@@ -201,5 +271,312 @@ fn pause() {
             ContinueArguments { thread_id: 0, }
         };
         let _: ContinueResponse = expect_message(&mut read);
+    });
+}
+
+#[test]
+fn breakpoints() {
+    run_debugger(|seq, stream, mut read| {
+        launch(stream, seq, "tests/main.glu");
+
+        let launch_response = expect_response!(read, LaunchResponse, "launch");
+        assert_eq!(launch_response.request_seq, *seq);
+        assert!(launch_response.success);
+
+        request! {
+            stream,
+            SetBreakpointsRequest,
+            "setBreakpoints",
+            *seq,
+            SetBreakpointsArguments {
+                breakpoints: Some(vec![
+                    SourceBreakpoint {
+                        column: None,
+                        condition: None,
+                        hit_condition: None,
+                        line: 1,
+                    },
+                    SourceBreakpoint {
+                        column: None,
+                        condition: None,
+                        hit_condition: None,
+                        line: 14,
+                    },
+                ]),
+                lines: None,
+                source: Source {
+                    path: Some("tests/main.glu".into()),
+                    .. Source::default()
+                },
+                source_modified: None,
+            }
+        };
+        expect_response!(read, SetBreakpointsResponse, "setBreakpoints");
+
+        request! {
+            stream,
+            ConfigurationDoneRequest,
+            "configurationDone",
+            *seq,
+            None
+        };
+        expect_response!(read, ConfigurationDoneResponse, "configurationDone");
+
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "breakpoint");
+
+        request! {
+            stream,
+            ContinueRequest,
+            "continue",
+            *seq,
+            ContinueArguments { thread_id: 0, }
+        };
+        expect_response!(read, ContinueResponse, "continue");
+
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "breakpoint");
+
+        request! {
+            stream,
+            ContinueRequest,
+            "continue",
+            *seq,
+            ContinueArguments { thread_id: 0, }
+        };
+        expect_response!(read, ContinueResponse, "continue");
+
+        expect_event!(read, TerminatedEvent, "terminated");
+    });
+}
+
+#[test]
+fn step_in() {
+    run_debugger(|seq, stream, mut read| {
+        launch(stream, seq, "tests/main.glu");
+
+        let launch_response = expect_response!(read, LaunchResponse, "launch");
+        assert_eq!(launch_response.request_seq, *seq);
+        assert!(launch_response.success);
+
+        request! {
+            stream,
+            SetBreakpointsRequest,
+            "setBreakpoints",
+            *seq,
+            SetBreakpointsArguments {
+                breakpoints: Some(vec![
+                    SourceBreakpoint {
+                        column: None,
+                        condition: None,
+                        hit_condition: None,
+                        line: 14,
+                    },
+                ]),
+                lines: None,
+                source: Source {
+                    path: Some("tests/main.glu".into()),
+                    .. Source::default()
+                },
+                source_modified: None,
+            }
+        };
+        expect_response!(read, SetBreakpointsResponse, "setBreakpoints");
+
+        request! {
+            stream,
+            ConfigurationDoneRequest,
+            "configurationDone",
+            *seq,
+            None
+        };
+        expect_response!(read, ConfigurationDoneResponse, "configurationDone");
+
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "breakpoint");
+
+        request! {
+            stream,
+            StepInRequest,
+            "stepIn",
+            *seq,
+            StepInArguments {
+                target_id: None,
+                thread_id: 0
+            }
+        };
+        expect_response!(read, StepInResponse, "stepIn");
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "step");
+
+        let (trace, _, _) = request_debug_info(seq, stream, read);
+        let frames = &trace.body.stack_frames;
+        assert_eq!(frames.len(), 2);
+        assert_eq!(frames[0].line, 6);
+        assert_eq!(frames[0].name, "test");
+        assert_eq!(frames[1].line, 14);
+
+        request! {
+            stream,
+            ContinueRequest,
+            "continue",
+            *seq,
+            ContinueArguments { thread_id: 0, }
+        };
+        expect_response!(read, ContinueResponse, "continue");
+
+        expect_event!(read, TerminatedEvent, "terminated");
+    });
+}
+
+#[test]
+fn step_out() {
+    run_debugger(|seq, stream, mut read| {
+        launch(stream, seq, "tests/main.glu");
+
+        let launch_response = expect_response!(read, LaunchResponse, "launch");
+        assert_eq!(launch_response.request_seq, *seq);
+        assert!(launch_response.success);
+
+        request! {
+            stream,
+            SetBreakpointsRequest,
+            "setBreakpoints",
+            *seq,
+            SetBreakpointsArguments {
+                breakpoints: Some(vec![
+                    SourceBreakpoint {
+                        column: None,
+                        condition: None,
+                        hit_condition: None,
+                        line: 6,
+                    },
+                ]),
+                lines: None,
+                source: Source {
+                    path: Some("tests/main.glu".into()),
+                    .. Source::default()
+                },
+                source_modified: None,
+            }
+        };
+        expect_response!(read, SetBreakpointsResponse, "setBreakpoints");
+
+        request! {
+            stream,
+            ConfigurationDoneRequest,
+            "configurationDone",
+            *seq,
+            None
+        };
+        expect_response!(read, ConfigurationDoneResponse, "configurationDone");
+
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "breakpoint");
+
+        request! {
+            stream,
+            StepOutRequest,
+            "stepOut",
+            *seq,
+            StepOutArguments {
+                thread_id: 0
+            }
+        };
+        expect_response!(read, StepOutResponse, "stepOut");
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "step");
+
+        let (trace, _, _) = request_debug_info(seq, stream, read);
+        let frames = &trace.body.stack_frames;
+        assert_eq!(frames.len(), 1);
+        assert_eq!(frames[0].line, 15);
+
+        request! {
+            stream,
+            ContinueRequest,
+            "continue",
+            *seq,
+            ContinueArguments { thread_id: 0, }
+        };
+        expect_response!(read, ContinueResponse, "continue");
+
+        expect_event!(read, TerminatedEvent, "terminated");
+    });
+}
+
+#[test]
+fn step_over() {
+    run_debugger(|seq, stream, mut read| {
+        launch(stream, seq, "tests/main.glu");
+
+        let launch_response = expect_response!(read, LaunchResponse, "launch");
+        assert_eq!(launch_response.request_seq, *seq);
+        assert!(launch_response.success);
+
+        request! {
+            stream,
+            SetBreakpointsRequest,
+            "setBreakpoints",
+            *seq,
+            SetBreakpointsArguments {
+                breakpoints: Some(vec![
+                    SourceBreakpoint {
+                        column: None,
+                        condition: None,
+                        hit_condition: None,
+                        line: 14,
+                    },
+                ]),
+                lines: None,
+                source: Source {
+                    path: Some("tests/main.glu".into()),
+                    .. Source::default()
+                },
+                source_modified: None,
+            }
+        };
+        expect_response!(read, SetBreakpointsResponse, "setBreakpoints");
+
+        request! {
+            stream,
+            ConfigurationDoneRequest,
+            "configurationDone",
+            *seq,
+            None
+        };
+        expect_response!(read, ConfigurationDoneResponse, "configurationDone");
+
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "breakpoint");
+
+        request! {
+            stream,
+            NextRequest,
+            "next",
+            *seq,
+            NextArguments {
+                thread_id: 0
+            }
+        };
+        expect_response!(read, StepOutResponse, "next");
+        let stopped = expect_event!(read, StoppedEvent, "stopped");
+        assert_eq!(stopped.body.reason, "step");
+
+        let (trace, _, _) = request_debug_info(seq, stream, read);
+        let frames = &trace.body.stack_frames;
+        assert_eq!(frames.len(), 1);
+        assert_eq!(frames[0].line, 15);
+
+        request! {
+            stream,
+            ContinueRequest,
+            "continue",
+            *seq,
+            ContinueArguments { thread_id: 0, }
+        };
+        expect_response!(read, ContinueResponse, "continue");
+
+        expect_event!(read, TerminatedEvent, "terminated");
     });
 }
